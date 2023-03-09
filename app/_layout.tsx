b22dfd90abee73a6e90dba1917666861f10cf20e
Sankalp Mukim <sankalpmukim@gmail.com>
2023-03-10 01:36:46 +0530

    feat: fixing tests

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from "expo-font";
 import { Slot, SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -39,8 +40,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <>
+    <SafeAreaView>
       <Slot />
-    </>
+    </SafeAreaView>
   );
 }
